253a7978217bbb10a0fc5c2ff88f7fc00606ce9c
H-akim <92632018+H-akim@users.noreply.github.com>
2023-03-04 15:51:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// var navlinks = document.getElementById("navlinks");
+// function show(){
+//     navlinks.style.right = "0";
+// }
+// function hide(){
+//     navlinks.style.right = "-200px";
+// }
